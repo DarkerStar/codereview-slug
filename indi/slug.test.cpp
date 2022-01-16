b3d9db986bbdaf0954be158e50d9bb68bc79b650
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2022-01-16 14:37:52 -0500

    Added test char traits type for basic_slug tests

--- indi/slug.test.cpp
+++ indi/slug.test.cpp
@@ -33,6 +33,42 @@
 
 namespace test_types {
 
+template <typename Char>
+struct test_char_traits
+{
+	using char_type = Char;
+	using int_type = typename std::char_traits<Char>::int_type;
+	using off_type = typename std::char_traits<Char>::off_type;
+	using pos_type = typename std::char_traits<Char>::pos_type;
+	using state_type = typename std::char_traits<Char>::state_type;
+
+	using stdtraits_t = std::char_traits<Char>;
+
+	static constexpr auto assign(char_type& r, char_type const& a) noexcept { return stdtraits_t::assign(r, a); }
+	static constexpr auto assign(char_type* p, std::size_t n, char_type a) { return stdtraits_t::assign(p, n, a); }
+
+	static constexpr auto eq(char_type a, char_type b) noexcept { return stdtraits_t::eq(a, b); }
+	static constexpr auto lt(char_type a, char_type b) noexcept { return stdtraits_t::lt(a, b); }
+
+	static constexpr auto move(char_type* dest, char_type const* src, std::size_t n) { return stdtraits_t::move(dest, src, n); }
+	static constexpr auto copy(char_type* dest, char_type const* src, std::size_t n) { return stdtraits_t::copy(dest, src, n); }
+
+	static constexpr auto compare(char_type const* s1, char_type const* s2, std::size_t n) { return stdtraits_t::compare(s1, s2, n); }
+
+	static constexpr auto length(char_type const* s) { return stdtraits_t::length(s); }
+
+	static constexpr auto find(char_type const* p, std::size_t n, char_type const& c) { return stdtraits_t::find(p, n, c); }
+
+	static constexpr auto to_char_type(int_type c) noexcept { return stdtraits_t::to_char_type(c); }
+	static constexpr auto to_int_type(char_type c) noexcept { return stdtraits_t::to_int_type(c); }
+
+	static constexpr auto eq_int_type(int_type c1, int_type c2) noexcept { return stdtraits_t::eq_int_type(c1, c2); }
+
+	static constexpr auto eof() noexcept { return stdtraits_t::eof(); }
+
+	static constexpr auto not_eof(int_type e) noexcept { return stdtraits_t::not_eof(e); }
+};
+
 template <typename Char, typename Traits>
 struct template_slug_policy
 {
@@ -43,11 +79,16 @@ struct template_slug_policy
 };
 
 using policies = std::tuple<
-	template_slug_policy<char, std::char_traits<char>>,
-	template_slug_policy<wchar_t, std::char_traits<wchar_t>>,
-	template_slug_policy<char8_t, std::char_traits<char8_t>>,
+	template_slug_policy<char,     std::char_traits<char>>,
+	template_slug_policy<wchar_t,  std::char_traits<wchar_t>>,
+	template_slug_policy<char8_t,  std::char_traits<char8_t>>,
 	template_slug_policy<char16_t, std::char_traits<char16_t>>,
-	template_slug_policy<char32_t, std::char_traits<char32_t>>
+	template_slug_policy<char32_t, std::char_traits<char32_t>>,
+	template_slug_policy<char,     test_char_traits<char>>,
+	template_slug_policy<wchar_t,  test_char_traits<wchar_t>>,
+	template_slug_policy<char8_t,  test_char_traits<char8_t>>,
+	template_slug_policy<char16_t, test_char_traits<char16_t>>,
+	template_slug_policy<char32_t, test_char_traits<char32_t>>
 >;
 
 } // namespace test_types
